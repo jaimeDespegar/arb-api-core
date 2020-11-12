5c5fb2cf230072764f89aa1a795d63a1bd1a3e4f
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-11-12 20:55:43 -0300

    wip

--- parkings/models/estadia.py
+++ parkings/models/estadia.py
@@ -9,7 +9,7 @@ class Estadia(models.Model):
     userName = models.CharField(max_length=50, default=None)
     isAnonymous = models.BooleanField(default=False)
     isActive = models.BooleanField(default=True)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, default=None, null=True)
 
     def __str__(self):
         return 'Estadia: user '+ self.userName + ', lugar  ' + str(self.placeUsed) + ', activa ' + str(self.isActive) + ', dateCreated ' + str(self.dateCreated)
--- parkings/views/estadiaView.py
+++ parkings/views/estadiaView.py
@@ -3,10 +3,11 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse, HttpResponse
 from rest_framework.response import Response
 from ..serializers import SegmentSerializer, EstadiaSerializer, PendingStaySerializer
-from ..models import Estadia, Segment, Place, BicycleParking, PendingStay
+from ..models import Estadia, Segment, Place, BicycleParking, PendingStay, BikeOwner
 from ..services.validator import Validator
 from ..services import EstadiaService
 from django.core import serializers
+from django.contrib.auth.models import User
 import json
 
 class EstadiaView():
@@ -177,6 +178,8 @@ class EstadiaView():
         for i in items:
             place=i.stay.place
             entrance = Segment.objects.get(estadia=i.stay)
+            user = User.objects.get(username=i.userName)
+            bikeOwner = BikeOwner.objects.get(user=user)
             item = {
               'userName': i.userName,
               'dateCreated': i.dateCreated,
@@ -187,7 +190,7 @@ class EstadiaView():
                 'description': place.bicycleParking.description              
               },
               'photos': {
-                'user': '',
+                'user': bikeOwner.profilePhoto,
                 'entrance': entrance.photoPath,
               }
             }
