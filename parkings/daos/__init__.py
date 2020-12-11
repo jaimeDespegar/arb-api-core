7dc63d054be37a0a80f291000f2c1116df833b19
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-12-11 10:26:13 -0300

    fixes daos

--- parkings/daos/__init__.py
+++ parkings/daos/__init__.py
@@ -5,7 +5,7 @@ from .segmentDao import SegmentDao
 from .pendingStayDao import PendingStayDao
 from .notificationEgressDao import NotificationEgressDao
 from .configurationDao import ConfigurationDao
-from ..userDao import UserDao
+from .userDao import UserDao
 
 
 __all__ = ['BicycleParkingDao', 'PlaceDao', 'StayDao', 'SegmentDao', 'PendingStayDao', 'NotificationEgressDao', 'ConfigurationDao', 'UserDao']
