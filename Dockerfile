b16788f16a6a365f800e7371573d235b789434ac
fjgalvan <fjgalvanx87@gmail.com>
2020-12-12 20:32:53 -0300

    docker + mysql

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
-FROM python:3.7
-
-ENV PYTHONUNBUFFERED 1 
-ENV PYTHONDONTWRITEBYTECODE=1
-
-RUN mkdir /code
-WORKDIR /code
-
-COPY requirements.txt /code/
-RUN python -m pip install -r requirements.txt
-
-COPY . /code/
+FROM python:3.6
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /my_app_dir
+WORKDIR /my_app_dir
+ADD requirements.txt /my_app_dir/
+RUN pip install --upgrade pip && pip install -r requirements.txt
+ADD . /my_app_dir/
--- api/settings.py
+++ api/settings.py
@@ -74,24 +74,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'nombreDB',
-#        'USER' : 'root',
-#        'PASSWORD' : 'root',
-#        'HOST' : 'localhost',
-#        'PORT' : ''
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parkings',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': 3306,
+    }
+}
 
 #DATABASES = {
 #    'default': {
